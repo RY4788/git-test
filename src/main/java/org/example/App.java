54825ea4fb1fd0b9d09f22136d368116e0616d86
RY4788 <97886394+RY4788@users.noreply.github.com>
2022-07-24 14:20:39 +0800

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println( "Hello World5!" );
         System.out.println( "Hello World5!" );
         System.out.println( "Hello World6!" );
+        System.out.println( "Hello World6!" );
     }
 }
